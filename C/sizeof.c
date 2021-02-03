c470a53e6f1e8851ce8aee23ac2f9427be05e259
Mukhametzyanov Nail <mukhametzyanovnm@gmail.com>
2021-02-04 01:51:56 +0300

    complete sizeof

--- C/sizeof.c
+++ C/sizeof.c
@@ -4,6 +4,15 @@
 
 int main() 
 {
-    printf("short int... \tsize: %d bytes %d байт \t", sizeof(short int));
-    printf("от %d до %d", SHRT_MAX, SHRT_MIN);
+    printf("\nshort int... \tsize: %d bytes \t", sizeof(short int));
+    printf("%d to %d", SHRT_MAX, SHRT_MIN);
+
+    printf("\nlong int... \tsize: %d bytes \t", sizeof(char));
+    printf("%d to %d", LONG_MAX, LONG_MIN);
+
+    printf("\nchar... \tsize: %d bytes \n", sizeof(char));
+    printf("float... \tsize: %d bytes \n", sizeof(float));
+    printf("double... \tsize: %d bytes \n", sizeof(double));
+
+    return 0;
 }

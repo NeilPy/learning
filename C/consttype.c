e14ff9ebec9d6582bbe5706f377451ff877d436d
Mukhametzyanov Nail <mukhametzyanovnm@gmail.com>
2021-02-09 23:50:03 +0300

    creating a const type

---
+++ C/consttype.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+
+int main()
+{
+    enum SNOOKER
+    {RED=1, YELLOW, GREEN, BROWN, BLUE, PINK, BLACK};
+    
+    enum SNOOKER pair = RED + BLACK;
+    printf("Pair value: %d\n", pair);
+
+    typedef unsigned short int USINT;
+    USINT num = 16;
+    printf("Usigned short int value: %d\n", num);
+
+    return 0;
+}

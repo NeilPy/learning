cc527dc3fd223a8bed27a1122694131eaa79fd62
Mukhametzyanov Nail <mukhametzyanovnm@gmail.com>
2021-02-17 17:27:17 +0300

    value assignment

---
+++ C/assign.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+
+int main()
+{
+    int a, b;
+
+    printf("Assigned: \n");
+    printf("\tVariable a = %d \n", a = 8);
+    printf("\tVariable b = %d \n", b = 4);
+    printf("Added & assigned: \n");
+    printf("\tVariable a += b (8 += 4) a = %d \n", a += b);
+    printf("Subtracted & assighed: \n");
+    printf("\tVariable a -= b (12 -= 4) a = %d \n", a -= b);
+
+    return 0;
+}
